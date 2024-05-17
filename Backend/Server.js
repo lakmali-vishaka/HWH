e949dcf8ec3e491155a1837c1bc57baca4750a97
lakmali-vishaka <119029100+lakmali-vishaka@users.noreply.github.com>
2024-05-17 14:57:13 +0530

    Update Server.js adding cors code again

--- Backend/Server.js
+++ Backend/Server.js
@@ -32,7 +32,16 @@ app.post('/pay', async(req, res) => {
 })
 
 // Use the required dependencies
-app.use(cors());
+app.use(cors(
+    {
+        origin: [https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST","GET"],
+        credentials: true
+                 }
+                 
+        
+    
+));
 
 
 
