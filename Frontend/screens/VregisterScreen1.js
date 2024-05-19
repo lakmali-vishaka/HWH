5f93ce4223cf3bd927350e7bb663f728b1044cbf
lakmali-vishaka <119029100+lakmali-vishaka@users.noreply.github.com>
2024-05-19 19:53:06 +0530

    Update VregisterScreen1.js

--- Frontend/screens/VregisterScreen1.js
+++ Frontend/screens/VregisterScreen1.js
@@ -1,100 +1,163 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
 
-
-
-
-
 const VregisterScreen1 = () => {
-  const [register_no, setRegisterNo] = useState('');
-  const [sv, setSv] = useState('');
+  const [vehicles, setVehicles] = useState([{ register_no: '', sv: '' }]);
   const navigation = useNavigation();
- 
-
-  // Backend connecting
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
 
+  const handleChange = (index, key, value) => {
+    const updatedVehicles = [...vehicles];
+    updatedVehicles[index][key] = value;
+    setVehicles(updatedVehicles);
+    
+  };
+
+  const addVehicle = () => {
+    if (vehicles.length < 5) {
+      setVehicles([...vehicles, { register_no: '', sv: '' }]);
+    } else {
+      alert('Maximum Vehicles Reached', 'You can only add up to 5 vehicles.');
+    }
+  };
+  
+
   const handleSubmit = async () => {
     try {
-      // Ensure both register_no and sv are not empty
-      if (!register_no || !sv) {
-        setError('Please fill in all the fields.');
-        return;
-      }
-
-      
-      // Generate QR code data
-      const qrCodeData = `Register Number: ${register_no}, Type: ${sv}`;
-      
-
-      // Set vehicleInfo values
-      const vehicleInfo = {
-        Vehicle_number: register_no,
-        Type: sv
-      };
-
-      const response = await axios.post('http:/192.168.43.116:8070/vehicle/addVehicle', vehicleInfo);
+      const promises = vehicles.map(async (vehicle) => {
+        const { register_no, sv } = vehicle;
+        if (!register_no || !sv) {
+          setError('Please fill in all the fields.');
+          return;
+        }
+  
+        // Include register_no and sv in the QR code data
+        const qrData = ` ${register_no}, ${sv}`;
+  
+        // Set the qrData property in the vehicle object
+        vehicle.qrData = qrData;
+  
+        const vehicleInfo = {
+          Vehicle_number: register_no,
+          Type: sv
+        };
+        const response = await axios.post('http:/192.168.43.116:8070/vehicle/addVehicle', vehicleInfo);
+        return response.data;
+      });
+  
+      const results = await Promise.all(promises);
       setMessage('Register successful!');
       setTimeout(() => {
-      navigation.push('VregisterScreen2', { P: register_no, p2: sv });
+        navigation.push('vregister1', { vehicles: vehicles });
       }, 2000);
     } catch (error) {
       setMessage('Error in Registration. Please try again.');
       console.error('Error:', error);
     }
+    
   };
+  
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <StatusBar style='dark'/>
-      <View className="bg-blue-950 w-full p-4 absolute flex-row justify-between" style={{marginTop:20}}>
-        <Icon name="arrow-left" size={18} color="#ffff" className="pt-1" onPress={()=> navigation.push('Home')}/>
-        <Text className="text-orange-400 text-center font-bold text-xl">  HighWay Hub</Text>
-        <Image source={require('../assets/images/profile.jpg')} className="w-6 h-6 rounded-3xl"/>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <StatusBar style='dark'/>
+        <View style={styles.header}>
+        <Icon name="arrow-left" size={18} color="#ffff" onPress={() => navigation.push('Home')} />
+        <Text style={styles.title}>HighWay Hub</Text>
+        <TouchableOpacity style={styles.profileIcon} onPress={() => navigation.push('user')}>
+          <Image source={require('../assets/images/profile.jpg')} style={styles.profileImage} />
+        </TouchableOpacity>
       </View>
 
-      <View style={{ backgroundColor: '#FF9100', padding: 2, marginTop: 100 }}>
-        <Text style={{ color: '#002366', textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>Vehicle Registration</Text>
+      <View className="w-full p-1 mt-5 flex-row justify-center" style={{backgroundColor:'#FF6F00'}}>
+        <Text className="text-center font-bold text-lg" style={{color:'#080742'}}>Vehicle Registration</Text>
       </View>
 
       <View style={{ alignItems: 'center', marginTop: 60 }}>
         <Icon name='car' size={150} color={'#191970'} />
       </View>
 
-      <View style={{ backgroundColor: '#C0C0C0', padding: 10, borderRadius: 20, alignSelf: 'center', marginTop: 40, width: 300 }}>
-        <TextInput placeholder='Register Number' placeholderTextColor={'gray'} onChangeText={setRegisterNo} />
-      </View>
+        {vehicles.map((vehicle, index) => (
+          <View key={index}>
+            {index > 0 && (
+  <View style={{ alignItems: 'center', marginTop: 60 }}>
+    <Text className="text-center font-bold text-lg" style={{color:'#080742'}}>Next Vehicle</Text>
+  </View>
+)}
 
-      <View style={{ backgroundColor: '#C0C0C0', padding: 10, borderRadius: 20, alignSelf: 'center', width: 300, marginTop: 30 }}>
-        <Picker
-          selectedValue={sv}
-          onValueChange={(itemValue, itemIndex) => setSv(itemValue)}
-          style={{ height: 50, width: 250, alignSelf:'center', marginTop:5 }}>
-        
-          <Picker.Item label="Select Type" value="" color='#002043' />
-          <Picker.Item label="Type 1" value="type 1" color='#002043' />
-          <Picker.Item label="Type 2" value="type 2" color='#002043' />
-          <Picker.Item label="Type 3" value="type 3" color='#002043' />
-        </Picker>
-      </View>
 
-      <View style={{ marginBottom: 2, height: 100, alignItems: 'center' }}>
-        <TouchableOpacity onPress={handleSubmit } style={{ backgroundColor: '#191970', borderRadius: 10, paddingVertical: 10, paddingHorizontal: 20, marginTop: 50, width: 300 }}>
-          <Text style={{ color: 'white', fontWeight: 'bold', alignSelf: 'center' }}>Register</Text>
+            <View style={{ backgroundColor: '#C0C0C0', padding: 10, borderRadius: 20, alignSelf: 'center', marginTop: 40, width: 300 }}>
+              <TextInput
+                placeholder='Register Number'
+                placeholderTextColor={'gray'}
+                onChangeText={(value) => handleChange(index, 'register_no', value)}
+              />
+            </View>
+
+            <View style={{ backgroundColor: '#C0C0C0', padding: 10, borderRadius: 20, alignSelf: 'center', width: 300, marginTop: 30 }}>
+              <Picker
+                selectedValue={vehicle.sv}
+                onValueChange={(value) => handleChange(index, 'sv', value)}
+                style={{ height: 50, width: 250, alignSelf:'center', marginTop:5 }}
+              >
+                <Picker.Item label="Select Type" value="" color='#002043' />
+                <Picker.Item label="Type 1" value="type 1" color='#002043' />
+                <Picker.Item label="Type 2" value="type 2" color='#002043' />
+                <Picker.Item label="Type 3" value="type 3" color='#002043' />
+              </Picker>
+            </View>
+          </View>
+        ))}
+
+        <TouchableOpacity onPress={addVehicle} style={{ backgroundColor: '#191970', borderRadius: 10, paddingVertical: 10, paddingHorizontal: 20, marginTop: 50, width: 300 ,alignSelf:'center'}}>
+          <Text style={{ color: 'white', fontWeight: 'bold', alignSelf: 'center' }}>Add Vehicle</Text>
         </TouchableOpacity>
-      </View>
-      
-      </View>
 
+        <TouchableOpacity onPress={handleSubmit} style={{ backgroundColor: '#191970', borderRadius: 10, paddingVertical: 10, paddingHorizontal: 20, marginTop: 20, width: 300,alignSelf:'center'}}>
+          <Text style={{ color: 'white', fontWeight: 'bold', alignSelf: 'center' }}>Register All Vehicles</Text>
+        </TouchableOpacity>
 
-   
+        
+        
+      </View>
+    </ScrollView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 25,
+  },
+  header: {
+    flexDirection: 'row',
+    backgroundColor: '#080742',
+    padding: 15,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#FF6F00',
+    fontSize: 20,
+  },
+  profileIcon: {
+    padding: 5,
+  },
+  profileImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+
+});
+
 export default VregisterScreen1;
+
